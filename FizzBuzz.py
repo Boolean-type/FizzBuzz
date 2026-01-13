aeae4b5f8fee4d206fc4047d37b044b60657eeda
Booleano <sergio23.tp@gmail.com>
2026-01-13 11:27:34 +0100

    Restricción de rango de números

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,22 +1,25 @@
 def fizzbuzz():
-    try:
-        n = int(input("Introduce un número entero positivo: "))
+    while True:
+        try:
+            n = int(input("Introduce un número entero entre 1 y 100: "))
 
-        if n <= 0:
-            print("Error: el número debe ser mayor que 0.")
-            return
+            if n < 1 or n > 100:
+                print("Error: el número debe estar entre 1 y 100.")
+                continue
 
-        for i in range(1, n + 1):
-            if i % 15 == 0:
-                print("FizzBuzz")
-            elif i % 3 == 0:
-                print("Fizz")
-            elif i % 5 == 0:
-                print("Buzz")
-            else:
-                print(i)
+            break 
 
-    except ValueError:
-        print("Error: debes introducir un número entero, no texto.")
+        except ValueError:
+            print("Error: debes introducir un número entero, no texto.")
+
+    
+    if n % 15 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+    else:
+        print(n)
 
 fizzbuzz()
